efae9d000174610e3a481a304f7345b44babe95a
luandevpro <luandevpro@gmail.com>
2019-03-17 15:24:13 +0700

    nextjs add

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "da9b0368e8613cde70ec";
+/******/ 	var hotCurrentHash = "2c487edf1766eaf2262f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
